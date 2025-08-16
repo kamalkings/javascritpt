1bbf703e4a521b6ec00ebd85651ae7b751683926
Kamalraj <dkamalraj@altimetrik.com>
2025-08-16 18:06:37 +0530

    control fow

--- scope_lab.js
+++ scope_lab.js
@@ -2,6 +2,7 @@
 var globalVar = "I'm a global variable";
 let globalLet = "I'm also global, but scoped with let";
 const globalConst = "I'm a global constant";
+console.log(globalVar);
 
 
 {
@@ -9,4 +10,7 @@ const globalConst = "I'm a global constant";
 var blockVar = "I'm a block-scoped var";
 let blockLet = "I'm a block-scoped let";
 const blockConst = "I'm a block-scoped const";
+}
+for(var i=0;i<5;i++){
+console.log(i);
 }
